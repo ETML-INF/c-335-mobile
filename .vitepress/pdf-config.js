ba8c52a6225311915057507c407d6cc8eb61e6b5
Jonathan Melly <jonathan.melly@gmail.com>
2025-12-21 10:24:04 +0100

    feat(pdf): add experimental mermaid support

--- .vitepress/pdf-config.js
+++ .vitepress/pdf-config.js
@@ -1,12 +1,23 @@
+/* courtesy of for mermaid integration
+  - https://gist.github.com/danishcake/d045c867594d6be175cb394995c90e2c
+  - https://github.com/MichielDeMey/marked-mermaid
+  - https://www.npmjs.com/package/@maddyguthridge/marked-mermaid
+  - https://dev.to/bmf_san/convert-markdown-format-files-to-pdf-files-mermaidemojitoc-compatible-5ekh*/
+const renderer = {
+    code(code, infostring) {
+        if (infostring === 'mermaid') {
+            return `<pre class="mermaid">${code}</pre>`
+        }
+        return false
+    },
+};
 
 module.exports = {
-    //Idée future: marked_extensions : mermaid
-    //https://github.com/emersonbottero/vitepress-plugin-mermaid/tree/main/src
-    //https://marked.js.org/using_pro#renderer
-    //https://github.com/mermaid-js/mermaid/issues/2972
-    //https://mermaid.js.org/config/usage.html#example-of-a-marked-renderer
-    //https://github.com/MichielDeMey/marked-mermaid/blob/main/src/index.js
-    //https://github.com/simonhaenisch/md-to-pdf/blob/master/src/lib/config.ts [fORK and update DEPS!!]
+    marked_extensions: [{ renderer }],
+    script: [
+        { url: 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js' },
+        { content: 'mermaid.initialize({ startOnLoad: false}); (async () => { await mermaid.run(); })();' }
+    ],
     pdf_options: {
         headerTemplate: "<div style='width:297mm;font-size:9px;font-style:italic;display:flex;justify-content: space-between;'>" +
             "<span style='margin-left:1cm'>ETML</span> " +
